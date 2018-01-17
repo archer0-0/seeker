9f259cec3bf0b9b74237a972e41dcefda46c3b27
maarq <maarq@DESKTOP-HNN8RC0>
2018-01-17 22:39:57 +0100

    Creation of all clases to to start the seeking of vulka. First step, to much to improve

--- seekerRoot/savior/src/main/java/com/arx/seeker/savior/SaviorApplication.java
+++ seekerRoot/savior/src/main/java/com/arx/seeker/savior/SaviorApplication.java
@@ -1,12 +1,29 @@
 package com.arx.seeker.savior;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
+import com.arx.seeker.savior.steps.Starter;
+
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class SaviorApplication {
+	@Autowired
+	Starter starter;
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(SaviorApplication.class, args);
+		ApplicationContext ctx= SpringApplication.run(SaviorApplication.class, args);
+		Starter starter=ctx.getBean(Starter.class);
+		starter.start();
 	}
+
+
+	
 }
